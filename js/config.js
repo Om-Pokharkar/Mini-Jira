9317c1ce473227467dc8cf41e5da12dc139f4174
Om Pokharkar <omnitin.pokharkar@gmail.com>
2025-11-13 13:34:15 +0530

    Replace old config with new Cognito settings
    Updated Cognito configuration with new values.

--- js/config.js
+++ js/config.js
@@ -1,10 +1,8 @@
-window._config = {
-  cognito: {
-    userPoolId: '',
-    userPoolClientId: '',
-    region: ''
-  },
-  api: {
-    invokeUrl: ''
-  }
+window._cognito = {
+    region: "ap-south-1",
+    userPoolId: "ap-south-1_DHkKLUmrb",
+    clientId: "70tas9h35fan4rvgl5q7tprh02",
+    domain: "https://ap-south-1dhkklumrb.auth.ap-south-1.amazoncognito.com",
+    redirectUri: "https://main.d1n2itgzbg5vvm.amplifyapp.com/",
+    logoutUri: "https://main.d1n2itgzbg5vvm.amplifyapp.com/"
 };
