a0fd837fb6fa09923e3bd325ee488953d3ff36a4
Om Pokharkar <omnitin.pokharkar@gmail.com>
2025-11-13 19:53:10 +0530

    Refactor API functions to use base URL

--- js/api.js
+++ js/api.js
@@ -1,25 +1,24 @@
-const API_URL = window._config.api.invokeUrl;
+// js/api.js
+const API_BASE = window._config.api.invokeUrl;
 
-export async function createBug(data) {
-    return fetch(`${API_URL}/createBug`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data)
-    }).then(res => res.json());
+async function apiPost(path, body) {
+  const token = sessionStorage.getItem('id_token');
+  const headers = { 'Content-Type': 'application/json' };
+  if (token) headers['Authorization'] = token; // or 'Bearer ' + token depending on your API authorizer
+  const resp = await fetch(`${API_BASE}${path}`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify(body)
+  });
+  return resp.json();
 }
 
-export async function assignBug(data) {
-    return fetch(`${API_URL}/assignBug`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data)
-    }).then(res => res.json());
+async function apiGet(path) {
+  const token = sessionStorage.getItem('id_token');
+  const headers = {};
+  if (token) headers['Authorization'] = token;
+  const resp = await fetch(`${API_BASE}${path}`, { headers });
+  return resp.json();
 }
 
-export async function closeBug(data) {
-    return fetch(`${API_URL}/closeBug`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data)
-    }).then(res => res.json());
-}
+export { apiPost, apiGet };
