6af7ddf84536060a34ea4ddcfa9998f2885bd69e
Om Pokharkar <omnitin.pokharkar@gmail.com>
2025-11-13 19:47:08 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,14 @@
-window._cognito = {
-    region: "ap-south-1",
+// js/config.js
+window._config = {
+  cognito: {
     userPoolId: "ap-south-1_DHkKLUmrb",
-    clientId: "70tas9h35fan4rvgl5q7tprh02",
+    userPoolClientId: "70tas9h35fan4rvgl5q7tprh02",
+    region: "ap-south-1",
     domain: "https://ap-south-1dhkklumrb.auth.ap-south-1.amazoncognito.com",
-    redirectUri: "https://main.d1n2itgzbg5vvm.amplifyapp.com/",
-    logoutUri: "https://main.d1n2itgzbg5vvm.amplifyapp.com/"
+    redirectUri: "https://main.d1n2itgzb5vwm.amplifyapp.com/",
+    logoutUri: "https://main.d1n2itgzb5vwm.amplifyapp.com/"
+  },
+  api: {
+    invokeUrl: "https://<YOUR_API_ID>.execute-api.ap-south-1.amazonaws.com"
+  }
 };
